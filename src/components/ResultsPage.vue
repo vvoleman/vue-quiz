173dd29fbf4261295b16297cea53d292765e1a7c
Memousek <namiskoczech@gmail.com>
2022-12-10 14:11:09 +0100

    container, úprava inputu

--- src/components/ResultsPage.vue
+++ src/components/ResultsPage.vue
@@ -1,16 +1,18 @@
 <template>
-    <h2>Váš výsledek je {{percentage}}% ({{correctAnswers}}/{{questions.length}})</h2>
+    <div class="results">
+        <h2>Váš výsledek je {{ percentage }}% ({{ correctAnswers }}/{{ questions.length }})</h2>
 
-    <h2>Výsledky:</h2>
-    <div v-for="i in questions.length" :key="i">
-        <h2>Otázka {{ i }}.: {{ questions[i-1].question }}</h2>
-        <li v-for="(option, j) in questions[i-1].options" :key="option">
-            <span v-if="j === answers[i-1]"><b>(Vaše odpověď) </b></span>
-            <span
-                :class="{'correct':j === questions[i-1].correct, 'incorrect': answers[i-1] === j && answers[i-1] !== questions[i-1].correct}">
-                {{ option }}
-            </span>
-        </li>
+        <h2>Výsledky:</h2>
+        <div v-for="i in questions.length" :key="i">
+            <h2>Otázka {{ i }}.: {{ questions[i - 1].question }}</h2>
+            <li v-for="(option, j) in questions[i - 1].options" :key="option">
+                <span v-if="j === answers[i - 1]"><b>(Vaše odpověď) </b></span>
+                <span
+                    :class="{ 'correct': j === questions[i - 1].correct, 'incorrect': answers[i - 1] === j && answers[i - 1] !== questions[i - 1].correct }">
+                    {{ option }}
+                </span>
+            </li>
+        </div>
     </div>
 </template>
 
@@ -22,15 +24,15 @@ export default {
         answers: Array,
     },
     computed: {
-        correctAnswers(){
+        correctAnswers() {
             let results = 0;
             for (let i = 0; i < this.questions.length; i++) {
-                results += (this.questions[i].correct === this.answers[i])*1;
+                results += (this.questions[i].correct === this.answers[i]) * 1;
             }
             return results;
         },
-        percentage(){
-            return Math.round((this.correctAnswers / this.questions.length)*10000)/100
+        percentage() {
+            return Math.round((this.correctAnswers / this.questions.length) * 10000) / 100
         }
     }
 }
@@ -38,9 +40,10 @@ export default {
 
 <style scoped>
 .correct {
-    color:green;
+    color: green;
     font-weight: bold;
 }
+
 .incorrect {
     color: #fd354c;
     font-weight: bold;
