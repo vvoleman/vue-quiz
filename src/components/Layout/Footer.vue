e9fa546773f841525487bbdb95e200da70dfd0c6
Memousek <namiskoczech@gmail.com>
2022-12-11 22:09:37 +0100

    footer

---
+++ src/components/Layout/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="footer">
+        <p>Stránka je stále ve vývoji. Pokud naleznete chybu, tak nám napište na <a href="https://m.me/kulisekmemo"
+                target="_blank">Messenger</a>. Děkujeme.
+        </p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "FooterComp",
+}
+</script>
+
+<style scoped lang="scss">
+.footer {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    height: 2.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    gap: .5rem;
+
+
+    text-align: center;
+}
+</style>
