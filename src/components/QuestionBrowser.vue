47cc81a4f0f6edba78a719ae6be1edc4eb1788d1
Memousek <namiskoczech@gmail.com>
2022-12-14 21:13:52 +0100

    tlačítka v1

--- src/components/QuestionBrowser.vue
+++ src/components/QuestionBrowser.vue
@@ -12,12 +12,24 @@
             </div>
         </div>
     </div>
-    <div class="question card card--animated stack">
-        <h3>{{ question.question }}</h3>
-        <div v-for="(option, i) in question.options" :key="option">
-            <input :id="i" type="radio" :name="currentQuestion" :value="i" v-model="answers[currentQuestion]"
-                @change="change">
-            <label :for="i">{{ option }}</label>
+    <div class="grid grid--center gird--fluid stack">
+        <div class="grid__row">
+            <div class="grid__col--24 row">
+                <button :key="i" @click="prevQuestion">
+                    Předchozí
+                </button>
+                <div class="question card card--animated stack">
+                    <h3>{{ question.question }}</h3>
+                    <div v-for="(option, i) in question.options" :key="option">
+                        <input :id="i" type="radio" :name="currentQuestion" :value="i"
+                            v-model="answers[currentQuestion]" @change="change">
+                        <label :for="i">{{ option }}</label>
+                    </div>
+                </div>
+                <button :key="i" @click="nextQuestion">
+                    Další
+                </button>
+            </div>
         </div>
     </div>
 </template>
@@ -50,21 +62,26 @@ export default {
             console.log('f');
             this.$emit('answer', this.answers)
         },
+        nextQuestion() {
+            if (this.currentQuestion + 1 >= this.questions.length) {
+                this.currentQuestion = 0;
+            } else {
+                this.currentQuestion++;
+            }
+        },
+        prevQuestion() {
+            if (this.currentQuestion - 1 < 0) {
+                this.currentQuestion = this.questions.length - 1;
+            } else {
+                this.currentQuestion--;
+            }
+        },
         nextItem(event) {
             if (event.keyCode === 37) {
-                if (this.currentQuestion - 1 < 0) {
-                    this.currentQuestion = this.questions.length - 1;
-                } else {
-                    this.currentQuestion--;
-                }
+                this.prevQuestion();
             } else if (event.keyCode === 39) {
-                if (this.currentQuestion + 1 >= this.questions.length) {
-                    this.currentQuestion = 0;
-                } else {
-                    this.currentQuestion++;
-                }
+                this.nextQuestion();
             }
-            console.log(this.currentQuestion)
         }
     }
 }
@@ -77,6 +94,11 @@ export default {
     flex-wrap: nowrap;
     gap: 5px;
     overflow: auto;
+}
+
+.row {
+    display: flex;
+    gap: 10px;
 }
 
 .question-box {
