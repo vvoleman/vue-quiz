bb595e9fc8b76c645e8a67d7dfea1f109da76319
Memousek <namiskoczech@gmail.com>
2023-01-07 17:57:31 +0100

    fujky

--- src/router/index.js
+++ src/router/index.js
@@ -14,11 +14,6 @@ const routes = [
     meta: {
       title: 'About'
     }
-  },
-  {
-    path: '/stats',
-    name: 'stats',
-    component: () => import(/* webpackChunkName: "about" */ '../views/StatsView.vue'),
   }
 ]
 
